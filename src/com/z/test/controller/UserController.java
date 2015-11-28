66bd2bd1f36867f07ac3cf874b62b2142c510994
z <z>
2015-11-28 15:09:31 +0800

    完善日志功能

--- src/com/z/test/controller/UserController.java
+++ src/com/z/test/controller/UserController.java
@@ -5,29 +5,34 @@ package com.z.test.controller;
  */
 
 
-        import javax.annotation.Resource;
-        import javax.servlet.http.HttpServletRequest;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
-        import com.z.test.pojo.User;
-        import com.z.test.service.IUserService;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.ResponseBody;
+import com.z.test.pojo.User;
+import com.z.test.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    static private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private IUserService userService;
 
     @RequestMapping("/showUser")
     @ResponseBody
-    public User toIndex(HttpServletRequest request,Model model){
+    public User toIndex(HttpServletRequest request, Model model) {
         int userId = Integer.parseInt(request.getParameter("id"));
         User user = this.userService.getUserById(userId);
         model.addAttribute("user", user);
+        logger.error("[info message]");
         return user;
     }
 }
